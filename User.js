19df81b934811d49ccd199b0b16ed5df815aed98
povlyna9899-bit <povlyna9899@gmail.com>
2026-03-12 16:55:00 +0700

    Add files via upload

--- User.js
+++ User.js
@@ -1,20 +1,36 @@
 const mongoose = require("mongoose");
 
 const userSchema = new mongoose.Schema({
-  username: {
-    type: String,
-    required: true,
-    unique: true
-  },
-  password: {
-    type: String,
-    required: true
-  },
-  role: {
-    type: String,
-    enum: ["admin", "player"],
-    default: "player" // 🔥 very important
-  }
-});
 
+  username:{
+    type:String,
+    unique:true,
+    required:true
+  },
+
+  password:{
+    type:String,
+    required:true
+  },
+
+  role:{
+    type:String,
+    enum:["superadmin","admin","player"],
+    default:"player"
+  },
+
+  parentAdmin:String,
+
+  spinsLeft:{
+    type:Number,
+    default:0
+  },
+
+  balance:{
+    type:Number,
+    default:0
+  }
+
+});
+const User = mongoose.model("User", userSchema);
 module.exports = mongoose.model("User", userSchema);
